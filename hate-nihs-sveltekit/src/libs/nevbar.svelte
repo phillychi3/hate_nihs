4164d002a392193eae9ffc5bda5aa52351a057b2
philly <phillychi3@gmail.com>
2021-12-19 19:12:20 +0800

    fuck

--- hate-nihs-sveltekit/src/libs/nevbar.svelte
+++ hate-nihs-sveltekit/src/libs/nevbar.svelte
@@ -20,23 +20,20 @@
   }
 </script>
 
-<div class = "navbar">
-  <Navbar color="light" light expand="md">
-    <NavbarBrand href="/">Hate nihs</NavbarBrand>
-    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-      <Nav class="ms-auto" navbar>
-        <NavItem>
-          <NavLink href="/send">發文</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/phillychi3">GitHub</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="/login">admin</NavLink>
-        </NavItem>
-
-      </Nav>
-    </Collapse>
-  </Navbar>
-</div>
+<Navbar color="light" light expand="md">
+  <NavbarBrand href="/">Hate nihs</NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink href="/send">發文</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="https://github.com/phillychi3">GitHub</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="/login">核彈按鈕</NavLink>
+      </NavItem>
+    </Nav>
+  </Collapse>
+</Navbar>
--- hate-nihs-sveltekit/src/routes/login.svelte
+++ hate-nihs-sveltekit/src/routes/login.svelte
@@ -1,6 +1,22 @@
 <script>
-    import Navbar from "../libs/nevbar.svelte";
-  </script>
-  
-  <Navbar />
-  
+  import Navbar from "../libs/nevbar.svelte";
+  let getdata = getimage();
+  async function getimage() {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(res);
+    }
+  }
+</script>
+
+<Navbar />
+<main>
+  {#await getdata}
+    <p>loading image</p>
+  {:then outdata}
+    <img src={outdata[0].url} alt="Dog image" />
+  {/await}
+</main>
