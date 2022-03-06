33aa1fc2566b6b3c3e4bef5f050ec3d9e25f4d69
philly <phillychi3@gmail.com>
2022-03-06 11:14:28 +0800

    vueeeeeeee

--- hate-nihs-sveltekit/src/libs/requests.js
+++
@@ -1,9 +0,0 @@
-
-const requests_data = () => {
-
-    const data = fetch("https://hatenihs.herokuapp.com/api/message/getdata")
-    .then(data => data.json()).catch(err => console.log(err))
-    return data; 
-}
-
-export default requests_data;
