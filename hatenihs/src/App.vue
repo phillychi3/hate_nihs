353a3103d8529c4d5863d79416d2cdff0f0aa3b4
philly <phillychi3@gmail.com>
2022-03-10 21:30:42 +0800

    fuck

--- hatenihs/src/App.vue
+++ hatenihs/src/App.vue
@@ -1,37 +1,68 @@
 <template>
-  <div id="app">
-    <b-container fluid class="hero">
-      <b-row>
-        <b-col cols="12">
-          <div class="hero--chooseHero">
-            <div class="hero--chooseHero__content">
-              <h1>IT幫幫忙 - 鐵人賽 30 天</h1>
-              <p>自討苦吃</p>
-            </div>
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
+    <section>
+        <div class="block">
+            <b-icon
+                icon="account"
+                size="is-small">
+            </b-icon>
+            <b-icon
+                icon="home"
+                size="is-small">
+            </b-icon>
+            <b-icon
+                icon="view-dashboard"
+                size="is-small">
+            </b-icon>
+        </div>
 
-    <b-navbar type="dark" variant="dark">
+        <div class="block">
+            <b-icon icon="account"></b-icon>
+            <b-icon icon="home"></b-icon>
+            <b-icon icon="view-dashboard"></b-icon>
+        </div>
 
-			<!-- 這邊使用 BV 支援 <router-link to=...>的元件 -->
-      <b-navbar-nav>
-        <b-nav-item to="/">Home</b-nav-item>
-        <b-nav-item to="/about">About Me</b-nav-item>
-      </b-navbar-nav>
+        <div class="block">
+            <b-icon
+                icon="account"
+                size="is-medium">
+            </b-icon>
+            <b-icon
+                icon="home"
+                size="is-medium">
+            </b-icon>
+            <b-icon
+                icon="view-dashboard"
+                size="is-medium">
+            </b-icon>
+        </div>
 
-			<!-- 這邊使用 BV navbar 的搜尋框元件，後面用以搜尋文章 -->
-      <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-      </b-navbar-nav>
-    </b-navbar>
+        <div class="block">
+            <b-icon
+                icon="account"
+                size="is-large"
+                type="is-success">
+            </b-icon>
+            <b-icon
+                icon="home"
+                size="is-large"
+                type="is-info">
+            </b-icon>
+            <b-icon
+                icon="view-dashboard"
+                size="is-large"
+                type="is-primary">
+            </b-icon>
+        </div>
 
-		<!-- 負責顯示所有內容 -->
-    <router-view/>
-
-  </div>
+        <div class="buttons">
+            <b-button
+                label="Finish"
+                type="is-dark"
+                icon-left="check" />
+            <b-button
+                label="Finish"
+                type="is-warning"
+                icon-left="check" />
+        </div>
+    </section>
 </template>
