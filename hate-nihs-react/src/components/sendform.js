30dfaa7508136c6f710bb8416a96dd8250c4fcfb
philly <phillychi3@gmail.com>
2022-05-16 01:59:40 +0800

    update react

--- hate-nihs-react/src/components/sendform.js
+++ hate-nihs-react/src/components/sendform.js
@@ -1,47 +1,81 @@
 import React from 'react';
 import {
  Form,
- Button,
 } from "react-bootstrap";
-import GoogleLogin from 'react-google-login';
+import { GoogleOAuthProvider,GoogleLogin } from '@react-oauth/google';
+import { useAlert } from 'react-alert'
 
-export default function Isform() {
-    const { useState } = React
-    const [value, setValue] = useState(),
-    onInput = ({target:{value}}) => setValue(value),
-    onFormSubmit = e => {
-      e.preventDefault()
-      console.log(value)
-      setValue()
-    }
-    const responseGoogle = (response) => {
-        console.log(response);
-    }
+export default function Isform() {  
+    const alert = useAlert()
 
+
+
+    const [formData, updateFormData] = React.useState();
+
+    const handleChange = (e) => {
+        updateFormData({
+          "data": e.target.value
+        });
+      };
+
+    const [formData2, updateFormData2] = React.useState();
+
+    const handleChange2 = (e) => {
+        updateFormData2({
+          "data": e.target.value
+        });
+      };
+
+    function testlogin(thing){
+        if(!formData || !formData2){
+            alert.error("請不要留空");
+            console.log("error");
+            return;
+        }
+        var jdata = {'jwt': thing, 'name':formData.data , "content": formData2.data};
+        fetch('http://localhost:5000/api/test/login', {
+            method: 'POST',
+            headers:  new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify(jdata)
+        })
+        .then(res => res.json())
+        .then(data => {
+            alert.success("發送成功");
+        })
+        .catch(err => {
+            alert.error("未知錯誤");
+            console.log(err)
+        })
+    }
 
     return (
-        <Form onSubmit={onFormSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
+        
+        <Form>
+        <GoogleOAuthProvider clientId="133157627833-551a92ekvsubhjh1arabm6hn16sjjlpl.apps.googleusercontent.com">
+            <Form.Group className="mb-3" controlId="formBasicname">
                 <Form.Label>暱稱</Form.Label>
-                <Form.Control type="email" placeholder="起輸入暱稱" onChange={onInput} value={value} />
+                <Form.Control type="text" placeholder="請輸入暱稱" name="name" onChange={handleChange}  />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>內容</Form.Label>
-                <Form.Control as="textarea" rows={3} type="password" placeholder="內容" />
+                <Form.Control as="textarea" rows={3} type="password" placeholder="內容" name="in" onChange={handleChange2}/>
             </Form.Group>
+
+            
             <GoogleLogin
-                clientId="133157627833-551a92ekvsubhjh1arabm6hn16sjjlpl.apps.googleusercontent.com"
-                buttonText="Login"
-                onSuccess={responseGoogle}
-                onFailure={responseGoogle}
-                cookiePolicy={'single_host_origin'}
-            />,
-            <Button variant="primary" type="submit">
-                發送
-            </Button>
+            onSuccess={credentialResponse => {                
+                testlogin(credentialResponse.credential)
+            }}
+            onError={() => {
+                console.log('Login Failed');
+            }}
+            useOneTap
+            />
+            </GoogleOAuthProvider>
         </Form>
 
         );
     }
-
--- hate-nihs-react/src/index.js
+++ hate-nihs-react/src/index.js
@@ -4,17 +4,26 @@ import './index.css';
 import Up from "./components/navbar";
 import Sform from "./components/sendform";
 import 'bootstrap/dist/css/bootstrap.css';
+import { positions, Provider ,transitions} from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+const options = {
+  timeout: 5000,
+  position: positions.TOP_RIGHT,
+  offset: '70px',
+  transition: transitions.FADE
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <div class="all">
       <Up />
       <div class="form">
+      <Provider template={AlertTemplate} {...options}>
         <Sform />
+      </Provider>
       </div>
-      
     </div>
-    
   </React.StrictMode>,
   document.getElementById('root')
 );
